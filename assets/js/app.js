e91ea39d1f8464569f02a83a487a81f9a900dd38
thierryHalot <thierry.halot@laposte.net>
2018-11-13 10:20:29 +0100

    mise en forme de la vue page de profil

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,27 @@ require('../css/app.scss');
 const $ = require('jquery');
 require('bootstrap');
 
+
 $("#testJs").append("<h1>Si ce titre est afficher, alors jquery est fonctionnel</h1>");
+
+$('#buttonMessageUpdateUser').click(
+
+
+
+    //fonction qui permet de cacher le nombre de point dans la page de profils
+    function messageHidden() {
+
+        var div = $('.conteneurMessageUpdateUser');
+
+        div.slideUp(800);
+
+
+    }
+
+
+);
+
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
